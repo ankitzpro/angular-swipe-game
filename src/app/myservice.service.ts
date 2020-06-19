31ab82b87ff49e9f25a33d8f24e69a2b27847540
ankitzpro <56755872+ankitzpro@users.noreply.github.com>
2020-06-19 16:40:36 +0530

    Update myservice.service.ts

--- src/app/myservice.service.ts
+++ src/app/myservice.service.ts
@@ -13,6 +13,7 @@ export class MyserviceService {
   i=0;
 intervalId: number = 0;
 seconds: number = 5;
+score=0;
 
   text="Compare the 2 words and decide whether there meaning is almost the same, almost the opposite, or they have another relationship";
     quesarray=[{'word1':'Tale','word2':'Yarn','answer':'same'},{'word1':'Eschew','word2':'Welcome','answer':'opposite'},
@@ -50,5 +51,24 @@ seconds: number = 5;
 
 
   clearTimer() { clearInterval(this.intervalId); }
+
+  getAnswer(eventtext){
+    switch(eventtext) { 
+          case 'left': { 
+            this.anstext=this.answer=='same' ? 'Correct Answer' : 'Incorrect Answer';
+             break; 
+          } 
+          case 'right': { 
+            this.anstext=this.answer=='opposite' ? 'Correct Answer' : 'Incorrect Answer';
+            break; 
+         } case 'down': { 
+          this.anstext=this.answer=='other' ? 'Correct Answer' : 'Incorrect Answer';
+          break; 
+          } 
+       }
+       if(this.anstext=='Correct Answer'){
+         this.score=this.score+this.seconds;
+       }
+  }
 
 }
