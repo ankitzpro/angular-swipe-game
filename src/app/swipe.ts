fdb862f1929e3c7485a66b02a9648c1849d8b31a
ankitzpro <mankit322@gmail.com>
2020-07-10 17:38:23 +0530

    1 file modified

--- src/app/swipe.ts
+++ src/app/swipe.ts
@@ -9,3 +9,8 @@ export const swipeleft = [
   style({ opacity: 1 }),
   style({ transform: 'translate3d(-200%, 0, 0) rotate3d(0, 0, 1, -120deg)', opacity: 0 }),
 ]
+
+export const swipedown = [
+    style({ opacity: 1 }),
+    style({ transform: 'translate3d(0, 200%, 0) rotate3d(0, 0, 1, 120deg)', opacity: 0 }),
+  ]
